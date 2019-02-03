0f42abcbd70126dd2d74a639af8c2721812ed3e9
Piotr Skowronek <p.skowronek90@gmail.com>
2019-02-03 14:40:03 +0100

    working alpha

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 255)->default(null);
+            $table->text('avatar', 255)->default(null);
+            $table->string('steam_id', 255)->default(null);
+            $table->string('points', 255)->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
