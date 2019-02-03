d1e2eee74ee05d9b0815c87283783607083f6327
Piotr Skowronek <p.skowronek90@gmail.com>
2019-02-03 17:29:07 +0100

    route fix

--- resources/views/_elements/account-data.blade.php
+++ resources/views/_elements/account-data.blade.php
@@ -3,6 +3,7 @@
     <img src="{{ $user->avatar }}" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">{{ $user->username }}</h5>
+        <p class="card-group"><b>@lang('SteamID'):</b>&nbsp;{{ $user->steam_id }}</p>
         <p class="card-group"><b>@lang('Utworzono'):</b>&nbsp;{{ $user->created_at }}</p>
         <p class="card-group"><b>@lang('Zebrane punkty'):</b>&nbsp;{{ $user->points }}</p>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@
 Route::get('/', 'DataController@index')->name('data.index');
 Route::get('show', 'DataController@show')->name('data.show');
 
-Route::get('/user/{id}', 'ApiController@getUserByIdOrSteamId');
-
 Route::get('auth/steam', 'AuthController@redirectToSteam')->name('auth.steam');
 Route::get('auth/steam/handle', 'AuthController@handle')->name('auth.steam.handle');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
